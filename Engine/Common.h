ad4d5f422fa36fdb918783fadc235aa7f5b16a29
Sam <Sam_Crook@live.co.uk>
2021-02-11 11:27:54 +0000

    LabRat 0.1 : Game So far
    Game so far with loading tile map, and sprites. Camera moves with WASD

--- Engine/Common.h
+++ Engine/Common.h
@@ -2,7 +2,16 @@
 
 #include <vector>
 
+#define SCREENwIDTH 1280
+#define SCREENHEIGHT 720
+
 #define TILEWIDTH 32
 #define TILEHEIGHT 32
 
+
+struct Vector2D
+{
+	float X;
+	float Y;
+};
 typedef std::vector<std::vector<int>> TileMap;
