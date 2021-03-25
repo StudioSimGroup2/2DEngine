c4d21a053fc8bb5edf19c660e13b3e89c9485d07
Cameron <cammyjbroon@gmail.com>
2021-03-25 10:52:43 +0000

    Ai Init Commit
    Initial Commit for AI

---
+++ Engine/AI.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <vector>
+#include "Common.h"
+
+using namespace std;
+
+class AI
+{
+	struct waypoint
+	{
+
+		int ID;
+		Vector2D pos;
+	};
+
+private:
+
+	vector<waypoint> waypoints;
+
+public:
+
+};
+
