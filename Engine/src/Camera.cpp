7ccc75010a806862cfec11041a7faef254b3e36b
Joe Bevan <44583084+Joe-Bevan@users.noreply.github.com>
2021-05-14 10:18:56 +0100

    Fixed deprecated function call issue

--- Engine/src/Camera.cpp
+++ Engine/src/Camera.cpp
@@ -9,15 +9,13 @@
 
 Camera* Camera::Create(glm::vec4 Eye, glm::vec4 At, glm::vec4 Up, float ViewWidth, float ViewHeight, const std::string& Name) 
 {
-	switch (Engine::Context::GetAPI()) 
-	{
-#if GRAPHICS_LIBRARY == 0
-	case Engine::Context::D3D11:	return new D3DCamera(Eye, At, Up, ViewWidth, ViewHeight, Name);
-#elif GRAPHICS_LIBRARY == 1
-	case Engine::Context::OpenGL:	return new OGLCamera(Eye, At, Up, ViewWidth, ViewHeight, Name);
-#endif
-	}
 
+#if GRAPHICS_LIBRARY == 0
+	return new D3DCamera(Eye, At, Up, ViewWidth, ViewHeight, Name);
+#elif GRAPHICS_LIBRARY == 1
+	return new OGLCamera(Eye, At, Up, ViewWidth, ViewHeight, Name);
+#endif
+	
 	ASSERT(false, "Could not find camera to match current API selection!");
 	return nullptr;
 }
